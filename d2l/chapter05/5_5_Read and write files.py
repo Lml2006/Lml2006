6f5dfde42b9886da7b7256b1a70a7b1d49065284
Lml2006 <2098255774@qq.com>
2026-07-22 22:10:54 +0800

    Add comments

--- d2l/chapter05/5_5_Read and write files.py
+++ d2l/chapter05/5_5_Read and write files.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 x = torch.arange(4)
 torch.save(x, 'x-file')
 x2 = torch.load('x-file')
-x2
+x2 
 y = torch.zeros(4)
 torch.save([x, y],'x-files')
 x2, y2 = torch.load('x-files')
