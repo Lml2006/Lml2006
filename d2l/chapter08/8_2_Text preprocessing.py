437059ca6ed211e34975749ef107580abcc52e08
Lml2006 <2098255774@qq.com>
2026-08-24 21:37:47 +0800

    Update 8_2_Text preprocessing.py

--- d2l/chapter08/8_2_Text preprocessing.py
+++ d2l/chapter08/8_2_Text preprocessing.py
@@ -9,7 +9,6 @@ def read_time_machine():  #@save
     with open(d2l.download('time_machine'), 'r') as f:
         lines = f.readlines()
     return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
-
 lines = read_time_machine()
 print(f'# 文本总行数: {len(lines)}')
 print(lines[0])
@@ -22,7 +21,6 @@ def tokenize(lines, token='word'):  #@save
         return [list(line) for line in lines]
     else:
         print('错误：未知词元类型：' + token)
-
 tokens = tokenize(lines)
 for i in range(11):
     print(tokens[i])
@@ -47,28 +45,22 @@ class Vocab:  #@save
             if token not in self.token_to_idx:
                 self.idx_to_token.append(token)
                 self.token_to_idx[token] = len(self.idx_to_token) - 1
-
     def __len__(self):
         return len(self.idx_to_token)
-
     def __getitem__(self, tokens):
         if not isinstance(tokens, (list, tuple)):
             return self.token_to_idx.get(tokens, self.unk)
         return [self.__getitem__(token) for token in tokens]
-
     def to_tokens(self, indices):
         if not isinstance(indices, (list, tuple)):
             return self.idx_to_token[indices]
         return [self.idx_to_token[index] for index in indices]
-
     @property
     def unk(self):  # 未知词元的索引为0
         return 0
-
     @property
     def token_freqs(self):
         return self._token_freqs
-
 def count_corpus(tokens):  #@save
     """统计词元的频率"""
     # 这里的tokens是1D列表或2D列表
@@ -92,6 +84,5 @@ for i in [0, 10]:
     if max_tokens > 0:
         corpus = corpus[:max_tokens]
     return corpus, vocab
-
 corpus, vocab = load_corpus_time_machine()
 len(corpus), len(vocab)
