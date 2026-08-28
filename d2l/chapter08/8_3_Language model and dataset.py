453851bb16dca9be187d59f05ee5bc1e1ede905e
Lml2006 <2098255774@qq.com>
2026-08-28 23:33:00 +0800

    Update 8_3_Language model and dataset.py

--- d2l/chapter08/8_3_Language model and dataset.py
+++ d2l/chapter08/8_3_Language model and dataset.py
@@ -32,11 +32,9 @@ def seq_data_iter_random(corpus, batch_size, num_steps):  #@save
     # 在随机抽样的迭代过程中，
     # 来自两个相邻的、随机的、小批量中的子序列不一定在原始序列上相邻
     random.shuffle(initial_indices)
-
     def data(pos):
         # 返回从pos位置开始的长度为num_steps的序列
         return corpus[pos: pos + num_steps]
-
     num_batches = num_subseqs // batch_size
     for i in range(0, batch_size * num_batches, batch_size):
         # 在这里，initial_indices包含子序列的随机起始索引
