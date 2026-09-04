7d6e5cb785d1d725f4722a24c8de6723cb909dcd
Lml2006 <2098255774@qq.com>
2026-09-04 19:50:18 +0800

    Update 9_2_Long Short-Term Memory network.py

--- d2l/chapter09/9_2_Long Short-Term Memory network.py
+++ d2l/chapter09/9_2_Long Short-Term Memory network.py
@@ -1,20 +1,16 @@
 import torch
 from torch import nn
 from d2l import torch as d2l
-
 batch_size, num_steps = 32, 35
 train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
 def get_lstm_params(vocab_size, num_hiddens, device):
     num_inputs = num_outputs = vocab_size
-
     def normal(shape):
         return torch.randn(size=shape, device=device)*0.01
-
     def three():
         return (normal((num_inputs, num_hiddens)),
                 normal((num_hiddens, num_hiddens)),
                 torch.zeros(num_hiddens, device=device))
-
     W_xi, W_hi, b_i = three()  # 输入门参数
     W_xf, W_hf, b_f = three()  # 遗忘门参数
     W_xo, W_ho, b_o = three()  # 输出门参数
