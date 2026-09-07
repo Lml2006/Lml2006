ec49875bdefd3019c958c8282a7471c1356d74e7
Lml2006 <2098255774@qq.com>
2026-09-07 19:19:40 +0800

    Update 9_3_Deep Recurrent Neural Network.py

--- d2l/chapter09/9_3_Deep Recurrent Neural Network.py
+++ d2l/chapter09/9_3_Deep Recurrent Neural Network.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from d2l import torch as d2l
-
 batch_size, num_steps = 32, 35
 train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
 vocab_size, num_hiddens, num_layers = len(vocab), 256, 2
