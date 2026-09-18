5b4b0d577afc8b349f05a749b3549a5b8754e8e1
Lml2006 <2098255774@qq.com>
2026-09-18 23:39:59 +0800

    Update 10_2_Attention Convergence: Nadaraya-Watson Kernel Regression.py

--- d2l/chapter10/10_2_Attention Convergence: Nadaraya-Watson Kernel Regression.py
+++ d2l/chapter10/10_2_Attention Convergence: Nadaraya-Watson Kernel Regression.py
@@ -38,7 +38,6 @@ class NWKernelRegression(nn.Module):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.w = nn.Parameter(torch.rand((1,), requires_grad=True))
-
     def forward(self, queries, keys, values):
         # queries和attention_weights的形状为(查询个数，“键－值”对个数)
         queries = queries.repeat_interleave(keys.shape[1]).reshape((-1, keys.shape[1]))
