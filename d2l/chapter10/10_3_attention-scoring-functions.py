4507b680b99325fd8e1c9f1049b30818971efb76
Lml2006 <2098255774@qq.com>
2026-09-20 22:32:25 +0800

    Update 10_3_attention-scoring-functions.py

--- d2l/chapter10/10_3_attention-scoring-functions.py
+++ d2l/chapter10/10_3_attention-scoring-functions.py
@@ -49,7 +49,6 @@ queries, keys = torch.normal(0, 1, (2, 1, 20)), torch.ones((2, 10, 2))
 values = torch.arange(40, dtype=torch.float32).reshape(1, 10, 4).repeat(
     2, 1, 1)
 valid_lens = torch.tensor([2, 6])
-
 attention = AdditiveAttention(key_size=2, query_size=20, num_hiddens=8,
                               dropout=0.1)
 attention.eval()
