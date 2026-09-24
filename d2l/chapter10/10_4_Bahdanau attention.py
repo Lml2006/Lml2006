0aff53dcac570c16ab935bb9a381b2902b6b3404
Lml2006 <2098255774@qq.com>
2026-09-24 10:22:47 +0800

    Update 10_4_Bahdanau attention.py

--- d2l/chapter10/10_4_Bahdanau attention.py
+++ d2l/chapter10/10_4_Bahdanau attention.py
@@ -6,7 +6,6 @@ class AttentionDecoder(d2l.Decoder):
     """带有注意力机制解码器的基本接口"""
     def __init__(self, **kwargs):
         super(AttentionDecoder, self).__init__(**kwargs)
-
     @property
     def attention_weights(self):
         raise NotImplementedError
@@ -21,13 +20,11 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
             embed_size + num_hiddens, num_hiddens, num_layers,
             dropout=dropout)
         self.dense = nn.Linear(num_hiddens, vocab_size)
-
     def init_state(self, enc_outputs, enc_valid_lens, *args):
         # outputs的形状为(batch_size，num_steps，num_hiddens).
         # hidden_state的形状为(num_layers，batch_size，num_hiddens)
         outputs, hidden_state = enc_outputs
         return (outputs.permute(1, 0, 2), hidden_state, enc_valid_lens)
-
     def forward(self, X, state):
         # enc_outputs的形状为(batch_size,num_steps,num_hiddens).
         # hidden_state的形状为(num_layers,batch_size,
@@ -53,7 +50,6 @@ class Seq2SeqAttentionDecoder(AttentionDecoder):
         outputs = self.dense(torch.cat(outputs, dim=0))
         return outputs.permute(1, 0, 2), [enc_outputs, hidden_state,
                                           enc_valid_lens]
-
     @property
     def attention_weights(self):
         return self._attention_weights
@@ -70,7 +66,6 @@ output.shape, len(state), state[0].shape, len(state[1]), state[1][0].shape
 embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
 batch_size, num_steps = 64, 10
 lr, num_epochs, device = 0.005, 250, d2l.try_gpu()
-
 train_iter, src_vocab, tgt_vocab = d2l.load_data_nmt(batch_size, num_steps)
 encoder = d2l.Seq2SeqEncoder(
     len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
